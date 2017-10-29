cc882b34a82472e49fd0de75bebc59797a62fc68
b <biro123@ntlworld.com>
2017-10-29 12:26:17 +0000

    PC04 - Added ball with initial velocity and rolling sound. Re-adjusted Camera position, re-organised folders.

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    public float launchSpeed = 250;
+
+    private Rigidbody rigidBody;
+    private AudioSource audioSource;
+
+	// Use this for initialization
+	void Start () {
+        rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+        Launch(launchSpeed);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Launch(float launchSpeed)
+    {
+        Debug.Log("moving ball - speed: " + launchSpeed.ToString());
+
+        rigidBody.velocity = new Vector3(0, 0, launchSpeed);
+
+        audioSource.Play();
+
+
+    }
+}
