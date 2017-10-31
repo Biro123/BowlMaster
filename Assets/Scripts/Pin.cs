2146dcae84f337720a1fab098243ce02a7e9d660
b <biro123@ntlworld.com>
2017-10-31 23:52:23 +0000

    PC09 - Added a swiper and pinsetter collider and script. Added standing pins counter (text)

---
+++ Assets/Scripts/Pin.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pin : MonoBehaviour {
+
+    public float standingThreshold = 10f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+
+    public bool IsStanding()
+    {
+        // eulerAngles are how you see them in unity - am angle from each vertex
+        Vector3 rotationInEuler = transform.rotation.eulerAngles;
+        float tiltInX = Mathf.Abs(rotationInEuler.x); // abs ensures positive only
+        float tiltInZ = Mathf.Abs(rotationInEuler.z);
+
+        if (tiltInX > 180) { tiltInX = 360f - tiltInX; }
+        if (tiltInZ > 180) { tiltInZ = 360f - tiltInZ; }
+
+        if (tiltInX <= standingThreshold && tiltInZ <= standingThreshold)
+        {
+            return true;
+        }
+        return false;
+    }
+
+}
---
+++ Assets/Scripts/PinSetter.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PinSetter : MonoBehaviour {
+
+    public Text standingDisplay;
+
+    private bool ballEnteredBox = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        standingDisplay.text = CountStanding().ToString();
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<Ball>())
+        {
+            ballEnteredBox = true;
+            standingDisplay.color = Color.red;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.GetComponent<Pin>())
+        {
+            Destroy(other.gameObject);
+        }
+    }
+
+    public int CountStanding()
+    {
+        int standingCount = 0;
+        Pin[] Pins = FindObjectsOfType<Pin>();
+
+        foreach (Pin currentPin in Pins)
+        {
+            if ( currentPin.IsStanding() )
+            {
+                standingCount++;
+            }
+        }
+        return standingCount;
+    }
+
+}
