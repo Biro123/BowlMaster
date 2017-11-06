ae0d6e35314928f61386d666ca1c9a0b924685eb
b <biro123@ntlworld.com>
2017-11-06 01:49:50 +0000

    PC13 - lots more test cases and code to handle all response states from ActionMaster

--- Assets/Editor/ActionMasterTest.cs
+++ Assets/Editor/ActionMasterTest.cs
@@ -7,11 +7,17 @@ using System.Collections;
 public class ActionMasterTest {
 
     private ActionMaster.Action endTurn = ActionMaster.Action.EndTurn;
+    private ActionMaster.Action tidy = ActionMaster.Action.Tidy;
+    private ActionMaster.Action reset = ActionMaster.Action.Reset;
+    private ActionMaster.Action endGame = ActionMaster.Action.EndGame;
+    private ActionMaster actionMaster;
 
-	[Test]
-	public void ActionMasterTestSimplePasses() {
-		// Use the Assert class to test conditions.
-	}
+
+    [SetUp]
+    public void Setup ()  // runs at the beginning of *each* test
+    {
+        actionMaster = new ActionMaster();
+    }
 
     [Test]
     public void T00_PassingTest()
@@ -22,7 +28,202 @@ public class ActionMasterTest {
     [Test]
     public void T01_OneStrikeReturnsEndTurn()
     {
-        ActionMaster actionMaster = new ActionMaster();
         Assert.AreEqual(endTurn, actionMaster.Bowl(10) );
     }
+
+    [Test]
+    public void T02_Bowl8ReturnsTidy()
+    {        
+        Assert.AreEqual(tidy, actionMaster.Bowl(8));
+    }
+
+    [Test]
+    public void T03_SecondBowlReturnsEndTurn()
+    {
+        actionMaster.Bowl(3);
+        Assert.AreEqual(endTurn, actionMaster.Bowl(1));
+    }
+
+    [Test]
+    public void T04_SpareReturnsEndTurn()
+    {
+        actionMaster.Bowl(3);
+        Assert.AreEqual(endTurn, actionMaster.Bowl(7));
+    }
+
+    [Test]
+    public void T05_Bowl0()
+    {
+        Assert.AreEqual(tidy, actionMaster.Bowl(0));
+    }
+
+
+    [Test]
+    public void T10_9thFrame_firstball()
+    {
+        for (int i = 1; i < 9; i++)  // 8 frames, no strikes or spares
+        {
+            actionMaster.Bowl(3);
+            actionMaster.Bowl(4);
+        }
+        Assert.AreEqual(tidy, actionMaster.Bowl(3));
+    }
+
+    [Test]
+    public void T11_9thFrame_noSpare()
+    {
+        for (int i = 1; i < 9; i++)  // 8 frames, no strikes or spares
+        {
+            actionMaster.Bowl(3);
+            actionMaster.Bowl(4);
+        }
+        actionMaster.Bowl(3);
+        Assert.AreEqual(endTurn, actionMaster.Bowl(5) );
+    }
+
+    [Test]
+    public void T12_9thFrame_Spare()
+    {
+        for (int i = 1; i < 9; i++)  // 8 frames, no strikes or spares
+        {
+            actionMaster.Bowl(3);
+            actionMaster.Bowl(4);
+        }
+        actionMaster.Bowl(3);
+        Assert.AreEqual(endTurn, actionMaster.Bowl(7) );
+    }
+
+    [Test]
+    public void T13_9thFrame_Strike()
+    {
+        for (int i = 1; i < 9; i++)  // 8 frames, no strikes or spares
+        {
+            actionMaster.Bowl(3);
+            actionMaster.Bowl(4);
+        }
+        Assert.AreEqual(endTurn, actionMaster.Bowl(10) );
+    }
+
+    [Test]
+    public void T20_10thFrame_3()
+    {
+        for (int i = 1; i < 10; i++)  // 9 frames, no strikes or spares
+        {
+            actionMaster.Bowl(3);
+            actionMaster.Bowl(4);
+        }
+        Assert.AreEqual(tidy, actionMaster.Bowl(3) );
+    }
+
+    [Test]
+    public void T21_10thFrame_53()
+    {
+        for (int i = 1; i < 10; i++)  // 9 frames, no strikes or spares
+        {
+            actionMaster.Bowl(3);
+            actionMaster.Bowl(4);
+        }
+        actionMaster.Bowl(5);
+        Assert.AreEqual(endGame, actionMaster.Bowl(3) );
+    }
+
+    [Test]
+    public void T22_10thFrame_Spare()
+    {
+        for (int i = 1; i < 10; i++)  // 9 frames, no strikes or spares
+        {
+            actionMaster.Bowl(3);
+            actionMaster.Bowl(4);
+        }
+        actionMaster.Bowl(9);
+        Assert.AreEqual(reset, actionMaster.Bowl(1) );
+    }
+
+    [Test]
+    public void T23_10thFrame_Spareandextra()
+    {
+        for (int i = 1; i < 10; i++)  // 9 frames, no strikes or spares
+        {
+            actionMaster.Bowl(3);
+            actionMaster.Bowl(4);
+        }
+        actionMaster.Bowl(9);
+        actionMaster.Bowl(1);
+        Assert.AreEqual(endGame, actionMaster.Bowl(6) );
+    }
+
+    [Test]
+    public void T24_10thFrame_Spareandextra10()
+    {
+        for (int i = 1; i < 10; i++)  // 9 frames, no strikes or spares
+        {
+            actionMaster.Bowl(3);
+            actionMaster.Bowl(4);
+        }
+        actionMaster.Bowl(4);
+        actionMaster.Bowl(6);
+        Assert.AreEqual(endGame, actionMaster.Bowl(10));
+    }
+    
+    [Test]
+    public void T25_10thFrame_Strike()
+    {
+        for (int i = 1; i < 10; i++)  // 9 frames, no strikes or spares
+        {
+            actionMaster.Bowl(3);
+            actionMaster.Bowl(4);
+        }
+        Assert.AreEqual(reset, actionMaster.Bowl(10));
+    }
+
+    [Test]
+    public void T26_10thFrame_Strikeandextra3()
+    {
+        for (int i = 1; i < 10; i++)  // 9 frames, no strikes or spares
+        {
+            actionMaster.Bowl(3);
+            actionMaster.Bowl(4);
+        }
+        actionMaster.Bowl(10);
+        Assert.AreEqual(tidy, actionMaster.Bowl(3) );
+    }
+
+    [Test]
+    public void T27_10thFrame_Strikeandextra35()
+    {
+        for (int i = 1; i < 10; i++)  // 9 frames, no strikes or spares
+        {
+            actionMaster.Bowl(3);
+            actionMaster.Bowl(4);
+        }
+        actionMaster.Bowl(10);
+        actionMaster.Bowl(3);
+        Assert.AreEqual(endGame, actionMaster.Bowl(5));
+    }
+
+    [Test]
+    public void T28_10thFrame_Strikeandextra10()
+    {
+        for (int i = 1; i < 10; i++)  // 9 frames, no strikes or spares
+        {
+            actionMaster.Bowl(3);
+            actionMaster.Bowl(4);
+        }
+        actionMaster.Bowl(10);
+        Assert.AreEqual(reset, actionMaster.Bowl(10) );
+    }
+
+    [Test]
+    public void T29_10thFrame_Strikeandextra1010()
+    {
+        for (int i = 1; i < 10; i++)  // 9 frames, no strikes or spares
+        {
+            actionMaster.Bowl(3);
+            actionMaster.Bowl(4);
+        }
+        actionMaster.Bowl(10);
+        actionMaster.Bowl(10);
+        Assert.AreEqual(endGame, actionMaster.Bowl(10));
+    }
+
 }
--- Assets/Scripts/ActionMaster.cs
+++ Assets/Scripts/ActionMaster.cs
@@ -6,24 +6,58 @@ public class ActionMaster : MonoBehaviour {
 
     public enum Action {Tidy, Reset, EndTurn, EndGame}
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private int[] bowls = new int[22];
+    private int bowl = 1;
+    private bool extraBowl = false;
+    
 
     public Action Bowl (int pins)
     {
         if (pins < 0 || pins > 10) { throw new UnityException("Invalid Pins value"); }
+        bowls[bowl] = pins;
 
-        if (pins == 10)
+        if (bowl >= 21)  {
+            return Action.EndGame;
+        }
+
+        if (bowl > 18) // last frame
         {
+            if (pins == 10) {  // Strike in last frame
+                bowl += 1;
+                extraBowl = true;
+                return Action.Reset;
+            }
+
+            if (bowl % 2 == 0)  {
+                if (pins + bowls[bowl-1] == 10 && pins != 0)   // Spare in last frame
+                {
+                    bowl += 1;
+                    extraBowl = true;
+                    return Action.Reset;
+                } else if (extraBowl == true)     // had strike in last frame 
+                {
+                    bowl += 1;
+                    return Action.Tidy;
+                }
+                return Action.EndGame;            // no strikes or spares in last frame 
+            }           
+        }
+
+        if (pins == 10)  // Strike
+        {
+            bowl += 2;
             return Action.EndTurn;
         }
+
+        if (bowl %2 == 0)    //remainder of (bowl / 2), ie end of frame
+        {
+            bowl += 1;
+            return Action.EndTurn;
+        } else  {            // First bowl in frame  
+            bowl += 1;
+            return Action.Tidy;
+        }
+
 
         throw new UnityException ("Not sure what action to return"); 
     }
